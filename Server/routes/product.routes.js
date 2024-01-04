4b046c7b12c0404cf6005d33754f8673c6275276
unknown <anubhavdixit268@gmail.com>
2024-01-04 18:30:54 +0530

    product get route modified-filter,sort,pagination added

--- Server/routes/product.routes.js
+++ Server/routes/product.routes.js
@@ -5,9 +5,60 @@ const productRouter = express.Router();
 //user routes
 
 productRouter.get("/", async (req, res) => {
+  let order, page, limit, from, till, pages, products;
+
+  if (req.query.order === "asc") {
+    order = 1;
+  } else if (req.query.order === "desc") {
+    order = -1;
+  }
+
+  let sort = req.query.sort;
+  let sortObj = {};
+  sortObj[sort] = order;
+
+  page = +req.query.page;
+  limit = +req.query.limit;
+
+  from = +req.query.from;
+  till = +req.query.till;
+
+  delete req.query.order;
+  delete req.query.sort;
+  delete req.query.page;
+  delete req.query.limit;
+  delete req.query.from;
+  delete req.query.till;
+
   try {
-    const products = await ProductModel.find();
-    res.status(200).send(products);
+    if (order && sort && from >= 0 && till > 0) {
+      products = await ProductModel.find({
+        $and: [
+          req.query,
+          { $and: [{ price: { $gte: from } }, { price: { $lte: till } }] },
+        ],
+      }).sort(sortObj);
+    } else if (from >= 0 && till > 0) {
+      products = await ProductModel.find({
+        $and: [
+          req.query,
+          { $and: [{ price: { $gte: from } }, { price: { $lte: till } }] },
+        ],
+      });
+    } else if (order && sort) {
+      products = await ProductModel.find(req.query).sort(sortObj);
+    } else {
+      products = await ProductModel.find(req.query);
+    }
+
+    if (page > 0 && limit > 0) {
+      pages = Math.ceil(products.length / limit);
+      products = products.slice((page - 1) * limit, page * limit);
+    } else if (page <= 0 || limit <= 0) {
+      products = [];
+    }
+
+    res.status(200).send([products, pages]);
   } catch (err) {
     res.status(400).send({ err: err.message });
   }
